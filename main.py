9d43395baf43062c3837cd715476f268cc35e508
akshayheda <akshayheda@gmail.com>
2019-01-21 21:58:43 -0800

    reduced how often face detection is done to reduce computation required

--- main.py
+++ main.py
@@ -15,28 +15,29 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     framecount+=1
 
-    avg += (len(faces))
     
     if ((framecount%15) == 0):
-        print(avg/15)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        framecount+=1
+        avg += (len(faces))
+        print(avg)
         framecount = 0
         avg = 0
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        cv2.imshow('frame',frame)
 
-    # Display the resulting frame
-    for (x,y,w,h) in faces:
-         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-         roi_gray = gray[y:y+h, x:x+w]
-         roi_color = frame[y:y+h, x:x+w]
-         eyes = eye_cascade.detectMultiScale(roi_gray)
-         for (ex,ey,ew,eh) in eyes:
-             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-    cv2.imshow('frame',frame)
+
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
