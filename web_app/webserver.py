a50ba568e134a0877e609f0454f13b20bb91bf34
jaredfitton <jaredfitton@gmail.com>
2019-02-11 11:04:41 -0800

    Added training pictures and reorganized files

---
+++ web_app/webserver.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from main import VehicleDetect
+app = Flask(__name__)
+
+detector = VehicleDetect()
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/getData')
+def getData():
+    return str(detector.dataCapture())
