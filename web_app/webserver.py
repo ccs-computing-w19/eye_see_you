347d17036ddd19592db2015e032fb1a1814a94aa
jaredfitton <jaredfitton@gmail.com>
2019-02-12 15:52:38 -0800

    Added image and text holder to index.html

--- web_app/webserver.py
+++ web_app/webserver.py
@@ -2,12 +2,12 @@ from flask import Flask
 from main import VehicleDetect
 app = Flask(__name__)
 
-detector = VehicleDetect()
+//detector = VehicleDetect()
 
 @app.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/getData')
-def getData():
-    return str(detector.dataCapture())
+# @app.route('/getData')
+# def getData():
+#     return str(detector.dataCapture())
