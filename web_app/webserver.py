d8e617bdb20d39f26b89c203f13b8cf8503cc2d4
jaredfitton <jaredfitton@gmail.com>
2019-03-04 14:55:26 -0800

    Setup python server. Added html/css. Enabled camera stream.

--- web_app/webserver.py
+++ web_app/webserver.py
@@ -1,13 +1,24 @@
-from flask import Flask
-from main import VehicleDetect
+from flask import Flask, redirect, url_for, session, request, jsonify, Markup, flash, render_template, Response
+from camera import VideoCamera
+
 app = Flask(__name__)
 
-//detector = VehicleDetect()
+# Change this to false in production
+app.debug = True
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return render_template('index.html')
 
-# @app.route('/getData')
-# def getData():
-#     return str(detector.dataCapture())
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+if __name__ == '__main__':
+    app.run()
