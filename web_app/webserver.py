addf430df42a2dd7284790430516eaae110e4526
jaredfitton <jaredfitton@gmail.com>
2019-03-05 14:35:45 -0800

    Added about page

--- web_app/webserver.py
+++ web_app/webserver.py
@@ -10,6 +10,10 @@ app.debug = True
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
